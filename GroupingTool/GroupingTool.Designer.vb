a6d59b0fa4cdeb551a6edb5aafcfb9230cf4d8bc
TianZhuang.Leo.Wang <1419473308@qq.com>
2023-12-06 18:30:04 +0900

    2023.12.06 First Commit. ZiMu

---
+++ GroupingTool/GroupingTool.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class GroupingTool
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        B_Group = New Button()
+        L_InputName = New Label()
+        TB_InputName = New RichTextBox()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Label2 = New Label()
+        TB_Num = New TextBox()
+        Label1 = New Label()
+        B_Clear = New Button()
+        Panel3 = New Panel()
+        TB_Group5 = New RichTextBox()
+        TB_Group4 = New RichTextBox()
+        TB_Group3 = New RichTextBox()
+        TB_Group2 = New RichTextBox()
+        TB_Group1 = New RichTextBox()
+        L_Group5 = New Label()
+        L_Group4 = New Label()
+        L_Group3 = New Label()
+        L_Group2 = New Label()
+        L_Group1 = New Label()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' B_Group
+        ' 
+        B_Group.FlatStyle = FlatStyle.Flat
+        B_Group.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        B_Group.Location = New Point(1617, 390)
+        B_Group.Margin = New Padding(5)
+        B_Group.Name = "B_Group"
+        B_Group.Size = New Size(200, 55)
+        B_Group.TabIndex = 6
+        B_Group.Text = "随机分组"
+        B_Group.UseVisualStyleBackColor = True
+        ' 
+        ' L_InputName
+        ' 
+        L_InputName.AutoSize = True
+        L_InputName.Font = New Font("微软雅黑", 10.5F, FontStyle.Regular, GraphicsUnit.Point)
+        L_InputName.Location = New Point(40, 40)
+        L_InputName.Margin = New Padding(5, 0, 5, 0)
+        L_InputName.Name = "L_InputName"
+        L_InputName.Size = New Size(390, 28)
+        L_InputName.TabIndex = 1
+        L_InputName.Text = "请在下面输入名字，并以空格进行分割。"
+        ' 
+        ' TB_InputName
+        ' 
+        TB_InputName.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_InputName.Location = New Point(40, 80)
+        TB_InputName.Margin = New Padding(5)
+        TB_InputName.Name = "TB_InputName"
+        TB_InputName.Size = New Size(1780, 300)
+        TB_InputName.TabIndex = 2
+        TB_InputName.Text = ""
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Panel2)
+        Panel1.Controls.Add(Panel3)
+        Panel1.Dock = DockStyle.Fill
+        Panel1.Location = New Point(0, 0)
+        Panel1.Margin = New Padding(5)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1848, 1021)
+        Panel1.TabIndex = 5
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Label2)
+        Panel2.Controls.Add(TB_Num)
+        Panel2.Controls.Add(Label1)
+        Panel2.Controls.Add(B_Group)
+        Panel2.Controls.Add(L_InputName)
+        Panel2.Controls.Add(TB_InputName)
+        Panel2.Controls.Add(B_Clear)
+        Panel2.Dock = DockStyle.Top
+        Panel2.Location = New Point(0, 0)
+        Panel2.Margin = New Padding(5)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(1848, 458)
+        Panel2.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(181, 405)
+        Label2.Margin = New Padding(5, 0, 5, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(192, 31)
+        Label2.TabIndex = 5
+        Label2.Text = "组    (最大输入5)"
+        ' 
+        ' TB_Num
+        ' 
+        TB_Num.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        TB_Num.ImeMode = ImeMode.Disable
+        TB_Num.Location = New Point(125, 406)
+        TB_Num.MaxLength = 1
+        TB_Num.Name = "TB_Num"
+        TB_Num.Size = New Size(48, 39)
+        TB_Num.TabIndex = 4
+        TB_Num.TextAlign = HorizontalAlignment.Center
+        TB_Num.WordWrap = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(40, 405)
+        Label1.Margin = New Padding(5, 0, 5, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 31)
+        Label1.TabIndex = 3
+        Label1.Text = "想分为"
+        ' 
+        ' B_Clear
+        ' 
+        B_Clear.FlatStyle = FlatStyle.Flat
+        B_Clear.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        B_Clear.Location = New Point(1617, 13)
+        B_Clear.Margin = New Padding(5)
+        B_Clear.Name = "B_Clear"
+        B_Clear.Size = New Size(200, 55)
+        B_Clear.TabIndex = 18
+        B_Clear.Text = "清空数据"
+        B_Clear.UseVisualStyleBackColor = True
+        ' 
+        ' Panel3
+        ' 
+        Panel3.Controls.Add(TB_Group5)
+        Panel3.Controls.Add(TB_Group4)
+        Panel3.Controls.Add(TB_Group3)
+        Panel3.Controls.Add(TB_Group2)
+        Panel3.Controls.Add(TB_Group1)
+        Panel3.Controls.Add(L_Group5)
+        Panel3.Controls.Add(L_Group4)
+        Panel3.Controls.Add(L_Group3)
+        Panel3.Controls.Add(L_Group2)
+        Panel3.Controls.Add(L_Group1)
+        Panel3.Dock = DockStyle.Bottom
+        Panel3.Location = New Point(0, 458)
+        Panel3.Margin = New Padding(5)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(1848, 563)
+        Panel3.TabIndex = 8
+        Panel3.Visible = False
+        ' 
+        ' TB_Group5
+        ' 
+        TB_Group5.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_Group5.Location = New Point(125, 380)
+        TB_Group5.Margin = New Padding(5)
+        TB_Group5.Name = "TB_Group5"
+        TB_Group5.ReadOnly = True
+        TB_Group5.Size = New Size(1698, 82)
+        TB_Group5.TabIndex = 16
+        TB_Group5.Text = ""
+        TB_Group5.Visible = False
+        ' 
+        ' TB_Group4
+        ' 
+        TB_Group4.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_Group4.Location = New Point(125, 290)
+        TB_Group4.Margin = New Padding(5)
+        TB_Group4.Name = "TB_Group4"
+        TB_Group4.ReadOnly = True
+        TB_Group4.Size = New Size(1698, 82)
+        TB_Group4.TabIndex = 14
+        TB_Group4.Text = ""
+        TB_Group4.Visible = False
+        ' 
+        ' TB_Group3
+        ' 
+        TB_Group3.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_Group3.Location = New Point(125, 200)
+        TB_Group3.Margin = New Padding(5)
+        TB_Group3.Name = "TB_Group3"
+        TB_Group3.ReadOnly = True
+        TB_Group3.Size = New Size(1698, 82)
+        TB_Group3.TabIndex = 12
+        TB_Group3.Text = ""
+        TB_Group3.Visible = False
+        ' 
+        ' TB_Group2
+        ' 
+        TB_Group2.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_Group2.Location = New Point(125, 110)
+        TB_Group2.Margin = New Padding(5)
+        TB_Group2.Name = "TB_Group2"
+        TB_Group2.ReadOnly = True
+        TB_Group2.Size = New Size(1698, 80)
+        TB_Group2.TabIndex = 10
+        TB_Group2.Text = ""
+        TB_Group2.Visible = False
+        ' 
+        ' TB_Group1
+        ' 
+        TB_Group1.Font = New Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        TB_Group1.Location = New Point(125, 20)
+        TB_Group1.Margin = New Padding(5)
+        TB_Group1.Name = "TB_Group1"
+        TB_Group1.ReadOnly = True
+        TB_Group1.Size = New Size(1698, 80)
+        TB_Group1.TabIndex = 8
+        TB_Group1.Text = ""
+        TB_Group1.Visible = False
+        ' 
+        ' L_Group5
+        ' 
+        L_Group5.AutoSize = True
+        L_Group5.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        L_Group5.Location = New Point(40, 380)
+        L_Group5.Margin = New Padding(5, 0, 5, 0)
+        L_Group5.Name = "L_Group5"
+        L_Group5.Size = New Size(77, 31)
+        L_Group5.TabIndex = 15
+        L_Group5.Text = "第5组"
+        L_Group5.Visible = False
+        ' 
+        ' L_Group4
+        ' 
+        L_Group4.AutoSize = True
+        L_Group4.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        L_Group4.Location = New Point(40, 290)
+        L_Group4.Margin = New Padding(5, 0, 5, 0)
+        L_Group4.Name = "L_Group4"
+        L_Group4.Size = New Size(77, 31)
+        L_Group4.TabIndex = 13
+        L_Group4.Text = "第4组"
+        L_Group4.Visible = False
+        ' 
+        ' L_Group3
+        ' 
+        L_Group3.AutoSize = True
+        L_Group3.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        L_Group3.Location = New Point(40, 200)
+        L_Group3.Margin = New Padding(5, 0, 5, 0)
+        L_Group3.Name = "L_Group3"
+        L_Group3.Size = New Size(77, 31)
+        L_Group3.TabIndex = 11
+        L_Group3.Text = "第3组"
+        L_Group3.Visible = False
+        ' 
+        ' L_Group2
+        ' 
+        L_Group2.AutoSize = True
+        L_Group2.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        L_Group2.Location = New Point(40, 110)
+        L_Group2.Margin = New Padding(5, 0, 5, 0)
+        L_Group2.Name = "L_Group2"
+        L_Group2.Size = New Size(77, 31)
+        L_Group2.TabIndex = 9
+        L_Group2.Text = "第2组"
+        L_Group2.Visible = False
+        ' 
+        ' L_Group1
+        ' 
+        L_Group1.AutoSize = True
+        L_Group1.Font = New Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        L_Group1.Location = New Point(40, 20)
+        L_Group1.Margin = New Padding(5, 0, 5, 0)
+        L_Group1.Name = "L_Group1"
+        L_Group1.Size = New Size(77, 31)
+        L_Group1.TabIndex = 7
+        L_Group1.Text = "第1组"
+        L_Group1.Visible = False
+        ' 
+        ' GroupingTool
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1848, 1021)
+        Controls.Add(Panel1)
+        Margin = New Padding(5)
+        MaximizeBox = False
+        Name = "GroupingTool"
+        Text = "GroupingTool"
+        Panel1.ResumeLayout(False)
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents B_Group As Button
+    Friend WithEvents L_InputName As Label
+    Friend WithEvents TB_InputName As RichTextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents B_Clear As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents L_Group5 As Label
+    Friend WithEvents L_Group4 As Label
+    Friend WithEvents L_Group3 As Label
+    Friend WithEvents L_Group2 As Label
+    Friend WithEvents L_Group1 As Label
+    Friend WithEvents TB_Group1 As RichTextBox
+    Friend WithEvents TB_Group4 As RichTextBox
+    Friend WithEvents TB_Group3 As RichTextBox
+    Friend WithEvents TB_Group2 As RichTextBox
+    Friend WithEvents TB_Group5 As RichTextBox
+    Friend WithEvents TB_Num As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ GroupingTool/GroupingTool.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Text
+
+Public Class GroupingTool
+    Private dt_GroupData As New DataTable
+
+    Private Sub GroupingTool_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 初始化DataTable
+        InitDataTable(dt_GroupData)
+    End Sub
+
+    ''' <summary>
+    ''' 随机分组
+    ''' </summary>
+    Private Sub B_Group_Click(sender As Object, e As EventArgs) Handles B_Group.Click
+
+        InitControls(False)
+        ' 分组数
+        Dim groupNum As Integer
+        If Fn_InputCheck(groupNum) = False Then Exit Sub
+
+        dt_GroupData.Clear()
+
+        ' 将人名拆分为单个名字
+        Dim names As String() = TB_InputName.Text.Replace(vbLf, " ").Split(" ", StringSplitOptions.RemoveEmptyEntries)
+
+        ' 生成不重复随机数,生成不重复随机数,并将随机数与人名存入DataTable
+        Fn_GetRandomSequence(names.Length, names.Length, names)
+
+        dt_GroupData.DefaultView.Sort = "random ASC"
+        dt_GroupData = dt_GroupData.DefaultView.ToTable
+
+        Dim groupPeopleNum As Integer = If(Int(names.Length / groupNum) = 0, 1, Int(names.Length / groupNum)) '每组平均分配的人数
+        Dim remainder As Integer = names.Length Mod groupNum '平均分配后的人数
+        Dim groupIndex As Integer = 1
+        Dim index As Integer = 1
+
+        ' 随机平均分配人名给小组
+        If remainder > 0 Then
+            Dim randoms As Integer() = Fn_GetRandomSequence(groupNum, remainder)
+            For i As Integer = 0 To remainder - 1
+                dt_GroupData.Rows(i).Item("groupNum") = randoms(i)
+            Next
+        End If
+
+        For i As Integer = 0 + remainder To names.Length - 1
+            If index > groupPeopleNum Then
+                groupIndex += 1
+                index = 1
+            End If
+            dt_GroupData.Rows(i).Item("groupNum") = groupIndex
+            index += 1
+        Next
+
+        ShowGroupData()
+
+    End Sub
+
+    Private Sub B_Clear_Click(sender As Object, e As EventArgs) Handles B_Clear.Click
+        InitControls()
+    End Sub
+
+    ''' <summary>
+    ''' 输入验证
+    ''' </summary>
+    ''' <returns>True:输入验证通过</returns>
+    Private Function Fn_InputCheck(ByRef groupNum As Integer) As Boolean
+        ' 分组数输入验证
+        If TB_Num.Text = String.Empty OrElse Integer.TryParse(TB_Num.Text, groupNum) = False OrElse groupNum > 5 Then
+            MessageBox.Show("分组数输入不正确，请重新输入。", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TB_Num.Focus()
+            TB_Num.SelectAll()
+            Return False
+        End If
+
+        ' 分组的名字验证
+        If TB_InputName.Text.Trim = String.Empty Then
+            MessageBox.Show("没有输入需要分组的名字，请重新输入。", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TB_InputName.Focus()
+            TB_Num.SelectAll()
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    ''' <summary>
+    ''' 生成不重复随机数,并将随机数与人名存入DataTable
+    ''' </summary>
+    ''' <param name="maxRandom">最大的随机数</param>
+    ''' <param name="randomCount">需要生成几个随机数</param>
+    ''' <param name="names">人名的List</param>
+    ''' <returns></returns>
+    Private Function Fn_GetRandomSequence(maxRandom As Integer, randomCount As Integer, Optional names As String() = Nothing) As Integer()
+
+        Dim sequence As Integer() = New Integer(maxRandom - 1) {}
+        Dim output As Integer() = New Integer(randomCount - 1) {}
+
+        For i As Integer = 0 To maxRandom - 1
+            sequence(i) = i
+        Next
+
+        Dim random As Random = New Random()
+        Dim _End As Integer = maxRandom - 1
+        Dim num As Integer
+
+        Dim DtRow As DataRow
+        '从N个数中随机取出一个数，与数组第N个数调换；
+        '代码通过 _End-1，把生成过的数用最后一个数代替，省去删除移动的开销。
+        For i As Integer = 0 To randomCount - 1
+            num = random.[Next](1, _End + 1)
+
+            If names IsNot Nothing Then
+                DtRow = dt_GroupData.NewRow()
+                DtRow("name") = names(i)
+                DtRow("random") = sequence(num)
+                dt_GroupData.Rows.Add(DtRow)
+            Else
+                output(i) = sequence(num)
+            End If
+
+            sequence(num) = sequence(_End)
+            _End -= 1
+
+        Next
+        Return output
+    End Function
+
+    ''' <summary>
+    ''' 初始化DataTable
+    ''' </summary>
+    ''' <param name="table">需要初始化的DataTable</param>
+    Private Sub InitDataTable(table As DataTable)
+        table.Columns.Add("groupNum", Type.GetType("System.Int32"))
+        table.Columns.Add("name")
+        table.Columns.Add("random", Type.GetType("System.Int32"))
+    End Sub
+
+    Private Sub ShowGroupData()
+        Dim lableName As Label
+        Dim richTextBoxName As RichTextBox
+        Dim value As String = String.Empty
+
+        Panel3.Visible = True
+        For i As Integer = 1 To CInt(TB_Num.Text)
+            lableName = Panel3.Controls("L_Group" & i.ToString)
+            lableName.Visible = True
+            richTextBoxName = Panel3.Controls("TB_Group" & i.ToString)
+            richTextBoxName.Visible = True
+
+            value = String.Empty
+            For Each dr As DataRow In dt_GroupData.Select("groupNum = " & i)
+                value += dr("name") & ","
+            Next
+            richTextBoxName.Text = value.TrimEnd(",")
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' 部分控件隐藏，并清空部分控件的值
+    ''' </summary>
+    ''' <param name="isAllClear">True:清空所有控件的值</param>
+    Private Sub InitControls(Optional isAllClear As Boolean = True)
+
+        If isAllClear Then
+            TB_Num.Text = String.Empty
+            TB_InputName.Text = String.Empty
+        End If
+
+        Panel3.Visible = False
+
+        TB_Group1.Text = String.Empty
+        TB_Group1.Visible = False
+        L_Group1.Visible = False
+
+        TB_Group2.Text = String.Empty
+        TB_Group2.Visible = False
+        L_Group2.Visible = False
+
+        TB_Group3.Text = String.Empty
+        TB_Group3.Visible = False
+        L_Group3.Visible = False
+
+        TB_Group4.Text = String.Empty
+        TB_Group4.Visible = False
+        L_Group4.Visible = False
+
+        TB_Group5.Text = String.Empty
+        TB_Group5.Visible = False
+        L_Group5.Visible = False
+    End Sub
+
+End Class
